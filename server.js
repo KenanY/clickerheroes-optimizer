3938e1fa726601be7e27a63f4f343ab7ee49ae8c
Kenan Yildirim <kenan@kenany.me>
2014-10-04 17:25:51 -0400

    Use `path.resolve` to create path to `static/`

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var static = require('node-static');
 var http = require('http');
+var path = require('path');
 
 var port = process.argv[2] || 3000;
-var file = new static.Server(__dirname + '/static');
+var file = new static.Server(path.resolve(__dirname, './static'));
 
 http.createServer(function(req, res) {
   req.addListener('end', function() {
