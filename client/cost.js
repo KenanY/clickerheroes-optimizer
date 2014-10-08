536bbb90cc31bbc172a663ff96f3e59e3279b28e
Kenan Yildirim <kenan@kenany.me>
2014-10-08 14:12:52 -0400

    Try out jeet

--- client/cost.js
+++ client/cost.js
@@ -1,3 +1,11 @@
+/**
+ * Get the cost of upgrading `hero` from level `from` to level `to`.
+ *
+ * @param  {Object} hero
+ * @param  {Number} from
+ * @param  {Number} to
+ * @return {Number}
+ */
 function cost(hero, from, to) {
   return hero.cost * Math.pow(1.07, from) * (Math.pow(1.07, to - from) - 1) / 0.07;
 }
